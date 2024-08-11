c291be62e9245f00f7b472ba94a52de991570dba
Jason <etho414.1@gmail.com>
2024-08-10 19:15:53 -0600

    Cleaned up init.lua file :

--- lua/etho/custom-remaps/init.lua
+++ lua/etho/custom-remaps/init.lua
@@ -1,3 +1,21 @@
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+
+-- Leader e is used for neo tree
+--vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+-- vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+-- vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+-- vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
 -- Alot of these keybinds are robbed from Primeagen (He the goat)
 -- Lets u move around text easily in visual mode
 
@@ -15,7 +33,7 @@ vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
 
 -- When pasting with leader P voids the current word selected then pastes
--- 	this preserves the current thing pasted
+--		this preserves the current thing pasted
 
 vim.keymap.set('x', '<leader>p', '"_dP')
 
